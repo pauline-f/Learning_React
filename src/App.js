d9843aa565af0f2f6981c86d6f4c63647f2cd5bd
pauline <pauline.feytel@gmail.com>
2019-01-25 13:37:27 +0100

    Passing method reference between components and adding two way binding

--- src/App.js
+++ src/App.js
@@ -7,21 +7,35 @@ class App extends Component {
   state = {
     persons: [
       { name:'Manu', age:29 },
-      { name:'Max', age:28 },
+      { name:'Max', age:28 }
     ]
   }
 
-  switchNameHandler = () => {
-    console.log('Was clicked!');
+  switchNameHandler = (newName) => {
+    this.setState( {
+      persons: [
+        { name:newName, age:29 },
+        { name:'Max', age:28 }
+      ]
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState( {
+      persons: [
+        { name: 'Manu', age:29 },
+        { name: event.target.value, age:28 }
+      ]
+    })
   }
 
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React app</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <button onClick={this.switchNameHandler.bind(this, 'Maximilian!')}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}  click={this.switchNameHandler.bind(this, 'Max!')}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
       </div>
     );
   }
